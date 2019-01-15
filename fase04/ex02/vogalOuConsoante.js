c96fba8c04f4d92c9e1cc2e18a6f9d814708e0be
nicolaom <46487002+nicolaom@users.noreply.github.com>
2019-01-15 10:32:04 -0200

    Update vogalOuConsoante.js

--- fase04/ex02/vogalOuConsoante.js
+++ fase04/ex02/vogalOuConsoante.js
@@ -1,4 +1,4 @@
-function vogalOuconsoante(letra) {
+function vogalOuConsoante(letra) {
     var objRegExp  = /^[A-Za-z]+$/
     if (objRegExp.test(letra) && letra == letra.substr(0, 1))
         return (letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u' || letra=='A' || letra=='E' || letra=='I' || letra=='O' || letra=='U') ? "Vogal" : "Consoante"
