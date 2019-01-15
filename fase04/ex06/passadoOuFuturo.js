611cd3b46ddd4e7949b8d0d4fe8d3d5ce7927b9a
nicolaom <46487002+nicolaom@users.noreply.github.com>
2019-01-15 11:10:50 -0200

    Update passadoOuFuturo.js

--- fase04/ex06/passadoOuFuturo.js
+++ fase04/ex06/passadoOuFuturo.js
@@ -1,8 +1,9 @@
 function passadoOuFuturo (data) {
-    var hoje = new Date(2015, 10, 21)
-    
+    var hoje = new Date('10/21/2015')
     if(data < hoje)
     return "Passado"
     else
     return "Futuro"
+    
+
 }
