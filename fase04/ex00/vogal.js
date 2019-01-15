00250674978b8feb8af596aadee9ceeab41b8a9b
nicolaom <46487002+nicolaom@users.noreply.github.com>
2019-01-15 10:44:21 -0200

    Update vogal.js

--- fase04/ex00/vogal.js
+++ fase04/ex00/vogal.js
@@ -1,6 +1,3 @@
 function vogal (letra) {
-    if(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u')
-    return "true"
-    else
-    return "false"
+    return(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u')
 }
