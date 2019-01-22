53a282cf8b9136737afe9810d565a8c5ef61de43
nicolaom <46487002+nicolaom@users.noreply.github.com>
2019-01-22 10:14:11 -0200

    Update primo.js

--- fase06/ex03/primo.js
+++ fase06/ex03/primo.js
@@ -1,12 +1,13 @@
 function primo(x) {
+    x = Math.abs(x)
 if (x==2||x==3||x==5||x==7)
     return "Sim"
 else if (x<=1||x!=Math.floor(x))
-    return "Não"
+    return "NÃ£o"
 else
 for (i=0;i<(Math.floor(x/2));i++) {
     if ((x/(i+2))==Math.floor(x/(i+2)))
-        return "Não"
+        return "NÃ£o"
     }
 return "Sim"
 }
