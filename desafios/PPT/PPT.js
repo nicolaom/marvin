aab185b126d847206394d18e86889ff5c253fa0b
nicolaom <46487002+nicolaom@users.noreply.github.com>
2019-07-18 20:55:34 -0300

    Isso ae

---
+++ desafios/PPT/PPT.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+  <html lang="pt-BR" dir="ltr">
+    <head>
+      <meta charset="utf-8" />
+      <title>Pedra, papel ou tesoura</title>
+    </head>
+    <body>
+      <div id="container">
+        <div id="options">
+          <img
+            onclick="pedra()"
+            src="https://www.goodfreephotos.com/albums/vector-images/grey-stone-rock-vector-clipart.png"
+            alt=""
+          />
+          <img
+            onclick="papel()"
+            src="https://marketveep.com/wp-content/uploads/2017/06/paper-min-500x415.png"
+            alt=""
+          />
+          <img
+            onclick="tesoura()"
+            src="https://requestreduce.org/images/school-scissors-clipart-9.png"
+            alt=""
+          />
+        </div>
+        <br />
+        <br />
+        <h1 id="resultado">Resultado</h1>
+      </div>
+    </body>
+  </html>
